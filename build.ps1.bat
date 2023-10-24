40ba5872b66d0b2be6d029407f698511d18b213b
Anton Pusch <github@anton-pusch.de>
2023-10-24 15:45:59 +0200

    Remove redundant build script

--- build.ps1.bat
+++
@@ -1,9 +0,0 @@
-# & cls & powershell -Command "Invoke-Command -ScriptBlock ([ScriptBlock]::Create(((Get-Content """%0""") -join """`n""")))" & exit
-# The above line makes the script executable when renamed .cmd or .bat
-
-$compiler = "$env:ProgramFiles\AutoHotkey 2\Compiler\Ahk2Exe.exe"
-$ahk2 = "$env:ProgramFiles\AutoHotkey 2\AutoHotkey64.exe"
-
-# Get-Process shortcutFox -ErrorAction SilentlyContinue | Stop-Process
-& $compiler /bin $ahk2 /in source/main.ahk /out bin\shortcutFox.exe /icon source\icons\menu.ico
-# Start-Process -FilePath "bin\shortcutFox.exe"
